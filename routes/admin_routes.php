f7a422e2c587eccde6cf4b3f5086295cdd8082b3
SakhiDeveloper-nextclick <nextclickdnd1@gmail.com>
2021-11-29 18:05:37 +0500

    add Admin And User Middleware

---
+++ routes/admin_routes.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\UserController;
+use App\Http\Controllers\Backend\DashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::middleware(['auth','Admin'])->group(function () {
+    /***
+     * Product Section
+     * */
+    Route::get('/dashboard',[DashboardController::class, 'index']);
+
+});
+?>
